72e8e0d6f97096f40fec975cdbc793d7b54ffb48
David Nanry <dnanry>
2018-05-09 19:59:46 -0500

    init vue app working with heroku

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# yapp
-Yet Another Planning Poker
+# YAPP - The Best Planning Poker Since ~~Sliced Br~~ the Last Planning Poker You've Seen
+
+### Development
+`yarn` to install, `yarn serve` to start server
+
+### Install your very own Heroku instance:
+ TBD
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$c: purple;
+h1 {
+  text-align: center;
+  color:      $c;
+  margin:     40px 0 0;
+}
+</style>
