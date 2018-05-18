80715c44fac6c67b24ddbf6dd76d490012e32a4b
David Nanry <dnanry>
2018-05-18 15:24:05 -0500

    feat: really cool Task item

--- src/components/Ballot.vue
+++ src/components/Ballot.vue
@@ -1,33 +1,38 @@
 <template>
-  <section v-on:click="ready = !ready"
-           class="ballot">
-    <h3 v-show="ready">Vote!</h3>
-    <h3 v-show="!ready">Voting Over</h3>
+  <main>
+    <Task v-bind:task='task'/>
+    <section v-on:click='ready = !ready'
+             class='ballot'>
+      <h3 v-show='ready'>Vote!</h3>
+      <h3 v-show='!ready'>Voting Over</h3>
 
-    <div class='vote-cards'>
-      <VoteCard v-bind:score="1" />
-      <VoteCard v-bind:score="2" />
-      <VoteCard v-bind:score="3" />
-      <VoteCard v-bind:score="5" />
-      <VoteCard v-bind:score="8" />
-      <VoteCard v-bind:score="13" />
-    </div>
-  </section>
+      <div class='vote-cards'>
+        <VoteCard v-bind:score='1' />
+        <VoteCard v-bind:score='2' />
+        <VoteCard v-bind:score='3' />
+        <VoteCard v-bind:score='5' />
+        <VoteCard v-bind:score='8' />
+        <VoteCard v-bind:score='13' />
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
+import Task     from './Task.vue'
 import VoteCard from './VoteCard.vue'
 
 export default {
   name:       'Ballot',
   components: {
+    Task,
     VoteCard
   },
   data: () => ({ ready: false })
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 $c: purple;
 h3 {
   text-align: center;
---
+++ src/components/Task.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h3 v-if="task === null" class='task-input-show task-input-show_null'>
+      Cool and fun task, man
+    </h3>
+    <h3 v-if="task !== null" class='task-input-show task-input-show_nonnull'>
+      {{ task }}
+    </h3>
+    <input v-model='task'
+           class='task-input'
+           placeholder='Enter a task'>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  data: () => ({ task: null })
+}
+</script>
+
+<style lang='scss' scoped>
+$c: purple;
+h3 {
+  color: $c;
+}
+.task {
+  border: 1px $c solid;
+}
+</style>
